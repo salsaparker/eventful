3bd7e211ab3230fb2c690dc24ecdedfa43ce5abf
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-12 15:03:38 -0600

    filled in most of bands controller, still need to add some paths and define band_params

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,22 +1,49 @@
 class BandsController < ApplicationController
 
   def index
-    @
-  end
-
-  def show
+    @bands = Band.all
   end
 
   def new
+    @band = Band.new
   end
 
   def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to PATH NAME
+    else
+      render :new
+    end
   end
 
   def edit
+   @band = Band.find(params[:id])
   end
 
   def update
+    @band = Band.find(params[:id])
+    if band.update(band_params)
+      redirect_to PATH NAME
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to PATH NAME
+  end
+
+private
+
+  def band_params
+
   end
 
 end
